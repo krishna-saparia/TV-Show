49bffb4f2344bdee2d26bfef6d286e2aed7fbd20
krishna saparia <krishnasaparia43045@gmail.com>
2020-11-05 13:59:48 -0500

    add jQuery Dependencies and add async function fetch data and show data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'TV-Show';
+
+  ngOnInit(): void {
+  }
+
+
+  // tslint:disable-next-line:typedef
+  async fetchData(){
+    const showDetails = await $.ajax('http://api.tvmaze.com/lookup/shows?tvrage=24493');
+    const search = $('#txtName').val();
+    console.log(showDetails);
+    if (search === showDetails.name.toLowerCase() || search === showDetails.name.toUpperCase()) {
+      this.showData(showDetails.name, showDetails.summary, showDetails.image.original);
+    } else {
+      alert('The show which are you looking for either not available or check your spelling ');
+    }
+  }
+
+
+  // tslint:disable-next-line:typedef
+    showData(name, summary, imgUrl){
+      // get main
+      const main = document.getElementById('main');
+
+      // create tags
+      const header = document.createElement('h1');
+      const img = document.createElement('img');
+      const summaryTag = document.createElement('div');
+
+      // add data
+      header.appendChild(document.createTextNode(name));
+      // header.appendChild(headText)
+      img.setAttribute('src', imgUrl);
+      img.setAttribute('width', '258');
+      summaryTag.innerHTML = summary;
+
+      // add to page
+      main.appendChild(header);
+      main.appendChild(img);
+      // main.appendChild(summaryTag)
+      main.innerHTML += summary;
+    }
+
 }
